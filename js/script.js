febd67477ed60a214109415bc8bd20b7f3288a93
wmcqueen9 <42362967+wmcqueen9@users.noreply.github.com>
2022-10-18 18:30:48 -0500

    Added Multiple users dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+const selectUserNumber=document.querySelector("#users")
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+  const data = await usersRequest.json();
+
+  const userResults = data.results; // array of objects
+  displayUsers(userResults);
+};
+
+getData(1);
+
+const displayUsers = function (userResults) {
+  // Clear the randomFolks element
+  randomFolks.innerHTML = "";
+
+  for (const user of userResults) {
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+    randomFolks.append(userDiv);
+  }
+};
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers=e.target.value;
+    getData(numUsers)
+
+
+})
